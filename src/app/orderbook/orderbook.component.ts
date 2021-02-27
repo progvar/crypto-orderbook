c76ccbed112a52d89fb25a7a9744cc5a3bd24cc0
progvar <prog.varadi@gmail.com>
2021-02-27 02:20:46 +0100

    feature: implement the connection to the WS API and the subsequent data processing

--- src/app/orderbook/orderbook.component.ts
+++ src/app/orderbook/orderbook.component.ts
@@ -1,30 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { ApiService } from './services/api/api.service';
 
 interface OrderbookDelta {
     price: number;
     size: number;
     total: number;
 }
-  
-const dataSource: OrderbookDelta[] = [
-    {price: 1, size: 100, total: 1234567, },
-    {price: 2, size: 100, total: 1234567, },
-    {price: 3, size: 100, total: 1234567, },
-    {price: 4, size: 100, total: 1234567, },
-    {price: 5, size: 100, total: 1234567, },
-    {price: 6, size: 100, total: 1234567, },
-    {price: 7, size: 100, total: 1234567, },
-    {price: 8, size: 100, total: 1234567, },
-    {price: 9, size: 100, total: 1234567, },
-    {price: 10, size: 100, total: 1234567, }
-]
 
 @Component({
   selector: 'app-orderbook',
   templateUrl: './orderbook.component.html',
-  styleUrls: ['./orderbook.component.scss']
+  styleUrls: ['./orderbook.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class OrderbookComponent {
-    dataSource = dataSource;
+export class OrderbookComponent  {
     displayedColumns: string[] = ['price', 'size', 'total'];
+    aksLevels = this.apiService.getAskLevels();
+    bidLevels = this.apiService.getBidLevels();
+
+    constructor(private apiService: ApiService) {}
 }
